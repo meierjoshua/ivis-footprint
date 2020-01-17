6ce57425ec01d02469c44a74c0fbe180a49f2256
Joshua Meier <joshua.meier@students.fhnw.ch>
2020-01-17 14:00:06 +0100

    Submission of project inlcuding documentation

--- src/world.js
+++ src/world.js
@@ -1,9 +1,10 @@
 const projection = d3.geoNaturalEarth1().scale(300).translate([900, 540]);
 const pathGenerator = d3.geoPath().projection(projection);
 const svg = d3.select('svg');
-const colorScaleBalance = d3.scaleLinear().domain([-8, -7, -0.00001, 0.00001, 7, 8]).range(['#f72525', '#f72525', '#ffB6B6', '#acc9ab', '#0d870b', '#0d870b']);
+//const colorScaleBalance = d3.scaleLinear().domain([-8, -7, -0.00001, 0.00001, 7, 8]).range(['#f72525', '#f72525', '#ffB6B6', '#acc9ab', '#0d870b', '#0d870b']);
+const colorScaleBalance = d3.scaleLinear().domain([-8, -7, -0.00001, 0.00001, 7, 8]).range(['#f72525', '#f72525', '#ffB6B6', '#E0FFFF', '#008B8B', '#008B8B']);
 const colorScaleFootprintPerPerson = d3.scaleLinear().domain([0, 7, 8]).range(['#ffD6D6', '#f90505', '#f90505']);
-const colorScaleBioCapacityPerPerson = d3.scaleLinear().domain([0, 7, 8]).range(['#9cc99b', '#0d870b', '#0d870b']);
+const colorScaleBioCapacityPerPerson = d3.scaleLinear().domain([0, 7, 8]).range(['#E0FFFF', '#008B8B', '#008B8B']);
 const tooltip = d3.select(".tooltip");
 let colorMode = "balanceBtn";
 const defs = svg.append("defs");
@@ -82,14 +83,14 @@ Promise.all([d3.json('data/world.geojson')])
                     .duration(200)
                     .style("opacity", .9);
 
-                tooltip.select(".populationGraph").remove();
-                tooltip.select(".footprintBioCapacityGraph").remove();
-                tooltip.select(".populationDevelopmentTitle").remove();
-                tooltip.select(".balanceDevelopmentTitle").remove();
+                tooltip.selectAll(".populationGraph").remove();
+                tooltip.selectAll(".footprintBioCapacityGraph").remove();
+                tooltip.selectAll(".populationDevelopmentTitle").remove();
+                tooltip.selectAll(".balanceDevelopmentTitle").remove();
 
                 tooltip
                     .style("left", (d3.event.pageX < 900 ? (d3.event.pageX + 5 + "px") : ((d3.event.pageX - 340) + "px")))
-                    .style("top", (d3.event.pageY < 400 ? (d3.event.pageY + 5 + "px") : ((d3.event.pageY - 420) + "px")))
+                    .style("top", (170 + "px"))
                     .style("display", "block");
 
 
@@ -99,6 +100,29 @@ Promise.all([d3.json('data/world.geojson')])
                 tooltip.select(".surfaceArea").text(d.properties.area.toLocaleString('de-CH'));
                 tooltip.select(".flag").attr("src", d.properties.flag_base64);
 
+
+                let worlds = (d.properties.nfa.filter(x => x.Year === "2016")[0].EFConsPerCap / 1.7).toFixed(2);
+                tooltip.select(".worldsNeededInput").text(worlds);
+
+                for (let i = 0; i < 9; ++i) {
+                    let clip;
+
+                    if (worlds < 0) {
+                        clip = "inset(0px 30px 0px 0px)";
+                    } else if (worlds >= 1) {
+                        clip = "inset(0px 0px 0px 0px)";
+                        worlds -= 1;
+                        console.log("fullw orld");
+                    } else {
+                        clip = `inset(0px ${(1 - worlds) * 30}px 0px 0px)`;
+                        worlds -= 1;
+                    }
+
+
+                    tooltip.select(`.worldImage:nth-child(${i + 1})`)
+                        .style("clip-path", clip);
+                }
+
                 tooltip.append("div").attr("class", "populationDevelopmentTitle").text("Development of Population (millions)");
 
                 const population = d.properties.population.filter(x => x.year < 2019);
@@ -106,7 +130,7 @@ Promise.all([d3.json('data/world.geojson')])
                     .attr("class", "populationGraph")
                     .append("g")
                     .attr("id", "populationGraph")
-                    .attr("transform", `translate(${50},${10})`);
+                    .attr("transform", `translate(${40},${10})`);
 
                 let xScale = d3.scaleLinear()
                     .domain([d3.min(_.map(population, c => parseInt(c.year))), d3.max(_.map(population, c => parseInt(c.year)))])
@@ -137,71 +161,10 @@ Promise.all([d3.json('data/world.geojson')])
                         .y(d => yScale(d.population / 1000))
                     );
 
-                let footprintBioCapacity = d.properties.nfa;
-                let efConsTotGHA = _.map(footprintBioCapacity, c => parseInt(c.EFConsTotGHA));
-                let biocapTotGHA = _.map(footprintBioCapacity, c => parseInt(c.BiocapTotGHA));
+                appendBioCapacityFootprintGraphToTooltip(tooltip, "BiocapTotGHA", "EFConsTotGHA", "Ecological Footprint vs Biocapacity - gha (millions)", d, xAxis, xScale, 1000000);
+                appendBioCapacityFootprintGraphToTooltip(tooltip, "BiocapPerCap", "EFConsPerCap", "Ecological Footprint vs Biocapacity - gha (per person)", d, xAxis, xScale, 1);
 
-                tooltip.append("div").attr("class", "balanceDevelopmentTitle").text("Ecological Footprint vs Biocapacity - gha (millions)");
 
-                let footprintBiocapacityDevelopmentSvg = tooltip.append("svg")
-                    .datum(footprintBioCapacity)
-                    .attr("class", "footprintBioCapacityGraph")
-                    .append("g")
-                    .attr("id", "footprintBioCapacityGraph")
-                    .attr("transform", `translate(${50},${10})`);
-
-                yScale = d3.scaleLinear()
-                    .domain([0, d3.max(efConsTotGHA.concat(biocapTotGHA)) / 1000000])
-                    .range([100, 0]);
-                yAxis = d3.axisLeft(yScale).ticks(6);
-
-                footprintBiocapacityDevelopmentSvg.append("g")
-                    .attr("id", "xAxis")
-                    .attr("transform", "translate(0," + 100 + ")")
-                    .call(xAxis);
-
-                footprintBiocapacityDevelopmentSvg.append("g")
-                    .attr("id", "yAxis")
-                    .call(yAxis);
-
-                footprintBiocapacityDevelopmentSvg.append("path")
-                    .datum(footprintBioCapacity)
-                    .attr("fill", "#9cc99b")
-                    .attr("d", d3.area()
-                        .x(d => xScale(d.Year))
-                        .y0(d => (parseInt(d.BiocapTotGHA) > parseInt(d.EFConsTotGHA)) ? yScale(d.BiocapTotGHA / 1000000) : yScale(d.EFConsTotGHA / 1000000))
-                        .y1(d => yScale(d.EFConsTotGHA / 1000000))
-                    );
-
-                footprintBiocapacityDevelopmentSvg.append("path")
-                    .datum(footprintBioCapacity)
-                    .attr("fill", "#ff9696")
-                    .attr("d", d3.area()
-                        .x(d => xScale(d.Year))
-                        .y0((d) => (parseInt(d.BiocapTotGHA) < parseInt(d.EFConsTotGHA)) ? yScale(d.EFConsTotGHA / 1000000) : yScale(d.BiocapTotGHA / 1000000))
-                        .y1(d => yScale(d.BiocapTotGHA / 1000000))
-                    );
-
-                footprintBiocapacityDevelopmentSvg.append("path")
-                    .datum(footprintBioCapacity)
-                    .attr("fill", "none")
-                    .attr("stroke", "#f72525")
-                    .attr("stroke-width", 1.5)
-                    .attr("d", d3.line()
-                        .x(d => xScale(d.Year))
-                        .y(d => yScale(d.EFConsTotGHA / 1000000))
-                    );
-
-                footprintBiocapacityDevelopmentSvg.append("path")
-                    .datum(footprintBioCapacity)
-                    .attr("fill", "none")
-                    .attr("stroke", "#0d870b")
-                    .attr("stroke-width", 1.5)
-                    .attr("d", d3.line()
-                        .x(d => xScale(d.Year))
-                        .y(d => yScale(d.BiocapTotGHA / 1000000)
-                        )
-                    );
             });
 
         d3.select("#zoomInBtn").on("click", () => mapZoom.scaleBy(svg.transition().duration(500), 1.5));
@@ -216,6 +179,75 @@ Promise.all([d3.json('data/world.geojson')])
 
         updateColorLegend();
     });
+
+function appendBioCapacityFootprintGraphToTooltip(tooltip, biocapacityDataString, footprintDataString, title, d, xAxis, xScale, divisor) {
+    debugger;
+    let footprintBioCapacity = d.properties.nfa;
+    let efConsTotGHA = _.map(footprintBioCapacity, c => parseFloat(c[footprintDataString]));
+    let biocapTotGHA = _.map(footprintBioCapacity, c => parseFloat(c[biocapacityDataString]));
+
+    tooltip.append("div").attr("class", "balanceDevelopmentTitle").text(title);
+
+    let footprintBiocapacityDevelopmentSvg = tooltip.append("svg")
+        .datum(footprintBioCapacity)
+        .attr("class", "footprintBioCapacityGraph")
+        .append("g")
+        .attr("id", "footprintBioCapacityGraph")
+        .attr("transform", `translate(${40},${10})`);
+
+    let yScale = d3.scaleLinear()
+        .domain([0, d3.max(efConsTotGHA.concat(biocapTotGHA)) / divisor])
+        .range([100, 0]);
+    let yAxis = d3.axisLeft(yScale).ticks(6);
+
+    footprintBiocapacityDevelopmentSvg.append("g")
+        .attr("id", "xAxis")
+        .attr("transform", "translate(0," + 100 + ")")
+        .call(xAxis);
+
+    footprintBiocapacityDevelopmentSvg.append("g")
+        .attr("id", "yAxis")
+        .call(yAxis);
+
+    footprintBiocapacityDevelopmentSvg.append("path")
+        .datum(footprintBioCapacity)
+        .attr("fill", "#E0FFFF")
+        .attr("d", d3.area()
+            .x(d => xScale(d.Year))
+            .y0(d => (parseFloat(d[biocapacityDataString]) > parseFloat(d[footprintDataString])) ? yScale(d[biocapacityDataString] / divisor) : yScale(d[footprintDataString] / divisor))
+            .y1(d => yScale(d[footprintDataString] / divisor))
+        );
+
+    footprintBiocapacityDevelopmentSvg.append("path")
+        .datum(footprintBioCapacity)
+        .attr("fill", "#ff9696")
+        .attr("d", d3.area()
+            .x(d => xScale(d.Year))
+            .y0((d) => (parseFloat(d[biocapacityDataString]) < parseFloat(d[footprintDataString])) ? yScale(d[footprintDataString] / divisor) : yScale(d[biocapacityDataString] / divisor))
+            .y1(d => yScale(d[biocapacityDataString] / divisor))
+        );
+
+    footprintBiocapacityDevelopmentSvg.append("path")
+        .datum(footprintBioCapacity)
+        .attr("fill", "none")
+        .attr("stroke", "#f72525")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(d => xScale(d.Year))
+            .y(d => yScale(d[footprintDataString] / divisor))
+        );
+
+    footprintBiocapacityDevelopmentSvg.append("path")
+        .datum(footprintBioCapacity)
+        .attr("fill", "none")
+        .attr("stroke", "#008B8B")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(d => xScale(d.Year))
+            .y(d => yScale(d[biocapacityDataString] / divisor)
+            )
+        );
+}
 
 function getCountryColor(d) {
     let minYear = d3.min(_.map(d.properties.nfa, c => parseInt(c.Year)));
@@ -258,7 +290,7 @@ function updateColorLegend() {
     switch (colorMode) {
         case "balanceBtn":
             colorScale = colorScaleBalance;
-            legendTooltip = "<div><b>ECOLOGICAL DEFICIT/RESERVE</b></div><div>An ecological deficit occurs when the Ecological Footprint of a population exceeds the biocapacityof the area available to that population. A national ecological deficit means that the nation is importing biocapacity through trade, liquidating national ecological assets or emitting carbon dioxide waste into the atmosphere. An ecological reserve exists when the biocapacity of a region exceeds its population's Ecological Footprint.</div><ul style=\"padding-left:15px;\"><li>Red: How many <b>times</b> footprint is greater than biocapacity</li><li>Green: How many <b>times</b> biocapacity is greater than footprint</li></ul>";
+            legendTooltip = "<div><b>ECOLOGICAL DEFICIT/RESERVE</b></div><div>An ecological deficit occurs when the Ecological Footprint of a population exceeds the biocapacityof the area available to that population. A national ecological deficit means that the nation is importing biocapacity through trade, liquidating national ecological assets or emitting carbon dioxide waste into the atmosphere. An ecological reserve exists when the biocapacity of a region exceeds its population's Ecological Footprint.</div><ul style=\"padding-left:15px;\"><li>Red: How many <b>times</b> footprint is greater than biocapacity</li><li>Blue: How many <b>times</b> biocapacity is greater than footprint</li></ul>";
             range = 18.75;
             break;
         case "footprintPerPersonBtn":
